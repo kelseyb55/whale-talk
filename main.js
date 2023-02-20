81264af52a497bb5edc45807bf6c8a4a5cb00096
Kelsey Bailey <123517600+kelseyb55@users.noreply.github.com>
2023-02-19 18:26:26 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+let input = 'Turpentine and Turtles';
+
+const vowels = ['a', 'e', 'i' ,'o','u' ];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+  //console.log(i);
+
+for (let j = 0; j < vowels.length; j++ ){
+ //console.log(j);
+ if (input[i] === vowels[j]){
+ 
+ 
+ if (input[i] === 'e') {
+   resultArray.push('ee');
+  
+   } if (input[i] === 'u'){
+ resultArray.push('uu');
+  
+  }  else { resultArray.push(input[i]);
+ }
+}
+ }
+}
+  
+ 
+ 
+
+console.log(resultArray.join('').toLowerCase());
